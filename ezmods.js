4bda15a5cc6f2cd0e4b6d328cbdda4bce09ef279
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-12 13:04:18 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -1,15 +1,15 @@
 new G.Unit({
-		name:'hacker',
-		desc:'@generates [insight] once a second<>A [dreamer]gives you hacks.',
-		icon:[1,2],
-		cost:{},
-		use:{'worker':1},
-		//upkeep:{'coin':0.1},
-		effects:[
-			{type:'gather',what:{'insight':1}},
-			{type:'gather',what:{'insight':1},req:{'stone-knapping':true}},
-		],
-		req:{'speech':true},
-		category:'discovery',
-		priority:100,
-	});
+	name:'hacker',
+	desc:'@generates [insight] once a second<>A [dreamer]gives you hacks.',
+	icon:[1,2],
+	cost:{},
+	use:{'worker':1},
+	upkeep:{'coin':0.1},
+	effects:[
+		{type:'gather',what:{'insight':1}},
+		{type:'gather',what:{'insight':1},req:{'stone-knapping':true}},
+	],
+	req:{'speech':true},
+	category:'discovery',
+	priority:100,
+});
