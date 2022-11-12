7b753293d1129f0c88837d9a22f8f33dc9134916
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-12 14:49:35 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -1,15 +1,24 @@
+G.AddData({
+	name:'hackers',
+	author:'me',
+	desc:'Adds a hacker unit, who generates fast ticks.',
+	engineVersion:1,
+	{   
 new G.Unit({
 	name:'hacker',
 	desc:'@generates [insight] once a second<>A [dreamer]gives you hacks.',
 	icon:[1,2],
 	cost:{},
 	use:{'worker':1},
-	upkeep:{'coin':0.1},
+	//upkeep:{'coin':0.1},
 	effects:[
 		{type:'gather',what:{'insight':1}},
-		{type:'gather',what:{'insight':1},req:{'stone-knapping':true}},
 	],
 	req:{'speech':true},
 	category:'discovery',
-	priority:100,
+	Priority:10,
+	});
+};	
 });
+
+
