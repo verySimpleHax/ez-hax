de6a82c43dba4a2c4267b406d9184b707398bc06
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-14 07:43:56 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -1,24 +1,29 @@
 G.AddData({
-	name:'hackers',
-	author:'me',
-	desc:'Adds a hacker unit, who generates fast ticks.',
-	engineVersion:1,
-	{   
-	new G.Unit({
-		name:'hacker',
-		desc:'@generates [insight] once a second<>A [dreamer]gives you hacks.',
-		icon:[1,2],
-		cost:{},
+    Name:'hacker Mod',
+    Author:'me',
+    desc:'This allows you to have a hacker!',
+    engineVersion:1,
+    manifest:'https://rawgit.com/Generatoror/hello-world/master/catManifest.js',
+    requires:['Default dataset*'],
+    sheets:{'magadadeeSheet':'/file:///C:/Users/s/Desktop/Screen%20Shot%202018-03-17%20at%203.36.22%20PM.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+    func:function()
+    {
+		new G.Unit({
+		name:'Magadadee',
+		desc:'@generates [insight] more frequently than a [dreamer]<>A [Magadadee] spends their time observing, thinking, and wondering why things are the way they are.',
+		icon:[0,0,'magadadeeSheet'],
+		cost:{'food':50},
 		use:{'worker':1},
-		upkeep:{'coin':0.1},
 		effects:[
-			{type:'gather',what:{'insight':1}},
+			{type:'gather',what:{'insight':0.9}},
+			{type:'gather',what:{'insight':0.9},req:{'symbolism':true}},
+			{type:'mult',value:3,req:{'symbolism':'on'}}
 		],
-		req:{'speech':true},
 		category:'discovery',
-		Priority:10,
-	});
-	}	
+		req:{'speech':true},
+		limitPer:{'house':1},
+		});
+    }
 });
 
 
