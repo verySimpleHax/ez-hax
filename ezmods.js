9382d7d26e9b4e1651219c71c43d972fba5d7e3d
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-14 13:08:37 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -1,22 +1,25 @@
 G.AddData({
-	name:'Hackers',
-	author:'hello world',
-	desc:'Adds a hacker unit, who generates fast ticks.',
+	name:'Monk',
+	author:'Shawn the mod maker',
+	desc:'Adds a monk unit, who generates fast ticks.',
 	engineVersion:1,
 	func:function()
 	{   
-        new G.Unit({
-            	name:'hacker',
-            	desc:'@generates fast ticks every now and then<>A [hacker] is one with the land and the natural energy of the universe.',
-            	icon:[1,2],
-            	cost:{},
-            	use:{'worker':1},
-            	upkeep:{'coin':0.2},
-            	effects:[
-			{type:'gather',what:{'insight':0.5}}
-            	],
-            	req:{'ritualism':true},
-            	category:'spiritual',
-        });
-	}
+       	new G.Unit({
+		name:'monk',
+		desc:'@generates [insight] more often than a dreamer, which you can use to research early technologies<>A [monk] spends their time thinking, and wondering why things are the way they are.',
+		icon:[1,2],
+		cost:{},
+		use:{'worker':1},
+		//upkeep:{'coin':0.2},
+		effects:[
+			{type:'gather',what:{'insight':0.3}},
+			{type:'gather',what:{'insight':0.15},req:{'symbolism':true}},
+			{type:'mult',value:1.2,req:{'wisdom rituals':'on'}}
+		],
+		req:{'speech':true},
+		category:'discovery',
+		priority:5,
+       });
+       }
 });
