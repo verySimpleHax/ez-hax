3c901de08e6224e01346f480dc5aa77babab3522
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-14 10:30:16 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -1,28 +1,30 @@
 G.AddData({
-    Name:'Magadoo Mod',
-    Author:'SomeToon',
-    desc:'This allows you to have a magadee from the magadadee clan in boggles creek in your tribe!',
-    engineVersion:1,
-    manifest:'https://rawgit.com/Generatoror/hello-world/master/catManifest.js',
-    requires:['Default dataset*'],
-    sheets:{'magadadeeSheet':'/file:///C:/Users/s/Desktop/Screen%20Shot%202018-03-17%20at%203.36.22%20PM.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
-    func:function()
-    {
-		new G.Unit({
-		name:'Magadadee',
-		desc:'@generates [insight] more frequently than a [dreamer]<>A [Magadadee] spends their time observing, thinking, and wondering why things are the way they are.',
-		icon:[0,0,'magadadeeSheet'],
-		cost:{'food':50},
-		use:{'worker':1},
-		effects:[
-			{type:'gather',what:{'insight':0.3}},
-			{type:'gather',what:{'insight':0.15},req:{'symbolism':true}},
-			{type:'mult',value:1.2,req:{'wisdom rituals':'on'}}
-		],
-		category:'discovery',
-		req:{'construction':true},
-		limitPer:{'house':1},
-		});
-    }
+	name:'Hackers',
+	author:'thatonedude',
+	desc:'Adds a hacker unit, who generates fast ticks and insight.',
+	engineVersion:1,
+	func:function()
+	{   
+        new G.Unit({
+            name:'hacker',
+            desc:'@generates fast ticks every now and then<>A [hacker] is one with the land and the natural energy of the universe.',
+            icon:[7,5],
+            cost:{},
+            use:{'worker':1},
+            upkeep:{'coin':0.2},
+            effects:[
+		    {type:'gather',what:{'insight':0.5}},
+		    {type:'function',func:(me)=>{
+                    let amount = me.amount - me.idle;
+                    // Limit of 25% chance per day, logarithmic
+                    if (.5 * (-1*((.875) ** amount) + 1) > Math.random()) {
+                        G.fastTicks++;
+                    };
+		}}
+            ],
+            req:{'ritualism':true},
+            category:'spiritual',
+        });
+	}
 });
 
