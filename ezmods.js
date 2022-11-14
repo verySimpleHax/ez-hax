acc523645cb6879963e4d01bf9b114e6373ff254
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-14 10:31:42 -0600

    Update ezmods.js

--- ezmods.js
+++ ezmods.js
@@ -13,8 +13,8 @@ G.AddData({
             use:{'worker':1},
             upkeep:{'coin':0.2},
             effects:[
-		    {type:'gather',what:{'insight':0.5}},
-		    {type:'function',func:(me)=>{
+		{type:'gather',what:{'insight':0.5}},
+		{type:'function',func:(me)=>{
                     let amount = me.amount - me.idle;
                     // Limit of 25% chance per day, logarithmic
                     if (.5 * (-1*((.875) ** amount) + 1) > Math.random()) {
