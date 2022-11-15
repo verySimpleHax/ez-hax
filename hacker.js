d8b8f68ad46bd991b228b2a9282324cbad52a977
verySimpleHax <99907741+verySimpleHax@users.noreply.github.com>
2022-11-15 10:25:13 -0600

    Update hacker.js

--- hacker.js
+++ hacker.js
@@ -1,11 +1,22 @@
 G.AddData({
-	name:'hackers',
-	author:'Owen "Ohead" Parker',
-	desc:'Adds a hacker unit, who generates fast ticks.',
-	engineVersion:1,
-	func:function()
-	{   
-		G.props['fastTicksOnResearch']=10000;
-		
-	}
+name:'Example mod',
+author:'Orteil',
+desc:'A simple example mod that adds hot peppers and hot sauce.',
+engineVersion:1,
+manifest:'modManifest.js',
+requires:['Default dataset*'],
+sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
+	new G.Res({
+		name:'get da food',
+		desc:'[get da food]s are loaded with happiness and, depending on who you ask, may produce a pleasant feelings when eaten.',
+		icon:[0,0,'spicySheet'],
+		turnToByContext:{'eat':{'health':0.9,'happiness':1},'decay':{'spoiled food':0.01}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+		partOf:'food',
+		category:'food',
+	});
+	
+	G.getDict('grass').res['gather']['get da food']=3;
+}
 });
